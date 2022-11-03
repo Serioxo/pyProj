54bab3c5cfb28fdc4c7db77f677e80e53d9ce2e8
Jakub <jakubmigac16@gmail.com>
2022-11-03 15:04:57 +0000

    ye

--- ticketsys/src/ticket.py
+++ ticketsys/src/ticket.py
@@ -1,16 +1,28 @@
 import os
 import random
 
+
+
 def GenerateNewTicket():
     ticketid = random.randint(1234,9999) + 1
     ticket = int(((ticketid * 16) / 2)) + 1
+    ticketList = []
+
+    ticketList.insert(1,ticket)
+
     fi = open("ticketDB", "a")
-    fi.write(str(ticket) + "\n")
+
+    if ticket == ticketList:
+        ticketid = random.randint(1234,9999) + 1
+        ticket = int(((ticketid * 16) / 2)) + 1 
+    else:
+
+        fi.write(str(ticket) + "\n")
+
     fi.close()
 
     return ticket
 
-def CheckIDConflict():
-    os.error(NotImplementedError)
+
 
 print("Ticket ID: " , GenerateNewTicket())
